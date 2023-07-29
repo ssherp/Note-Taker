1c72f52fae7b6fc1df80b7627268f2813ddae450
ssherp <ssherpa89@gmail.com>
2023-07-28 21:36:02 -0700

    update

--- server.js
+++ server.js
@@ -3,37 +3,46 @@
 const express = require('express');
 const path = require('path');
 const notesData = require('./db/db.json');
-
+const fs = require("fs")
 const PORT = 3001;
-
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 
+app.use(express.static('public'));
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+
+
+
+app.get('/api/notes', (req, res) => {
+  res.json(notesData);
+});
+// res.json(notesData));
+
 //route for index.html
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 //route for note.html
 
-app.get('/api/notes', (req, res) => res.json(notesData));
+
 
 app.post('/api/notes', (req, res) => {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    const newNotes = req.body;
-  
-    console.log(newNotes);
-  
-    // We then add the json the user sent to the character array
-    notesData.push(newNotes);
-  
-    // We then display the JSON to the users
-    res.json(newNotes);
-  });
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  req.body.id=uuidv4()
+  const newNotes = req.body;
+
+  // We then add the json the user sent to the character array
+  notesData.push(newNotes);
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+  // We then display the JSON to the users
+  res.json(notesData);
+
+});
 
 
 app.listen(PORT, () => {
